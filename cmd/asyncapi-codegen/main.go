537d07de09ab055c9529b2d8b7cf67c3453e2f42
bwarminski <bwarminski@gmail.com>
2023-10-28 23:37:10 -0400

    Revert "Testing local setup"
    This reverts commit aac14a5a641d106aef8e8937e735a9594eba1fbb.

--- cmd/asyncapi-codegen/main.go
+++ cmd/asyncapi-codegen/main.go
@@ -3,32 +3,31 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/lerenn/asyncapi-codegen/pkg/codegen"
 )
 
 func run() int {
-	//flags := ProcessFlags()
+	flags := ProcessFlags()
 
-	fmt.Fprintf(os.Stderr, "I work!")
-	return 255
+	cg, err := codegen.FromFile(flags.InputPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		return 255
+	}
 
-	//cg, err := codegen.FromFile(flags.InputPath)
-	//if err != nil {
-	//	fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-	//	return 255
-	//}
-	//
-	//opt, err := flags.ToCodegenOptions()
-	//if err != nil {
-	//	fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-	//	return 255
-	//}
-	//
-	//if err := cg.Generate(opt); err != nil {
-	//	fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-	//	return 255
-	//}
-	//
-	//return 0
+	opt, err := flags.ToCodegenOptions()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		return 255
+	}
+
+	if err := cg.Generate(opt); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		return 255
+	}
+
+	return 0
 }
 
 func main() {
